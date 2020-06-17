5e05c9cc6933f4d9c17df4a07ed6d224431ca643
MaFranks <throwaway@kodakvalet.com>
2020-06-16 18:43:58 -0700

    Updates

---
+++ precache-manifest.d83152acfe048eafa58928a3a6780f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0321d5b5363c6cf04abe3b6e6e625209",
+    "url": "/REACT-todosimple/index.html"
+  },
+  {
+    "revision": "15c801e1386b153df516",
+    "url": "/REACT-todosimple/static/css/main.e785260d.chunk.css"
+  },
+  {
+    "revision": "ccd61e9b333d9ac9dcd6",
+    "url": "/REACT-todosimple/static/js/2.986cdae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT-todosimple/static/js/2.986cdae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c801e1386b153df516",
+    "url": "/REACT-todosimple/static/js/main.34745e3d.chunk.js"
+  },
+  {
+    "revision": "65c639c5442f6cc2afb1",
+    "url": "/REACT-todosimple/static/js/runtime-main.63053354.js"
+  }
+]);
